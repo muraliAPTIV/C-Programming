74689f3897dc5fd19a15706091e583d0b5881a7f
bfc2a0 <murali.krishna.gajula@aptiv.com>
2024-02-15 14:00:19 +0530

    deleted

--- htmlreport.py
+++
@@ -1,4 +0,0 @@
-contents = open("C:\\Flashing\\Smoke_test\\SITS_TC_Result.txt","r")
-with open("SITS_TC_Test.html", "w") as e:
-    for lines in contents.readlines():
-        e.write("<pre>" + lines + "</pre> <br>\n")
